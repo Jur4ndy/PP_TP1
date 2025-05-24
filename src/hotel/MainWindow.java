cba3c2aa2e1717b94ba57ea26c079573b8e4b418
Jur4ndy <jurandy.junior@ufam.edu.br>
2025-05-24 01:46:34 -0400

    solved the allignment issue, changed kevins's image for a png of chapter 3's icon, added the buttons, still gotta add the windows those buttons will take you to

--- src/hotel/MainWindow.java
+++ src/hotel/MainWindow.java
@@ -12,45 +12,65 @@ public class MainWindow extends JFrame {
 	//Baseado no capítulo 3 de Celeste
 	public MainWindow () {
 		super("Celestial Resort Hotel");
-		
-		ImageIcon  mainIcon = new ImageIcon(getClass().getResource("Kevin.png"));	//Main window icon
-		Image img = mainIcon.getImage(); // transform it 
-		img = img.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH);
-		mainIcon = new ImageIcon(img);
-		this.setIconImage(mainIcon.getImage());
-		this.getContentPane().setBackground(new Color(29, 22, 26));
-		setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
-		
-		JLabel logo = new JLabel();
-		logo.setIcon(mainIcon);
-		logo.setBounds(0, 10, 2800, 100);
-		
-		JLabel title = new JLabel("Welcome to the world-renowed Celestial Resort Hotel!");
-		title.setBounds(0, 10, 2800, 200);
-	    title.setForeground(new Color(100, 100, 100));
-	    title.setFont(new Font("MV Boli", Font.PLAIN, 50)); 
-		
-	    JLabel ref = new JLabel("yes, this is a Celeste reference");
-		ref.setBounds(400, 40, 280, 100);
-	    ref.setHorizontalAlignment(SwingConstants.CENTER);
-	    ref.setForeground(new Color(95, 100, 100));
-	    ref.setFont(new Font("MV Boli", Font.ITALIC, 20));
-	    
-	    JButton 
-	    
-	    JPanel buttonPanel = new JPanel();
-	    buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
-	    
-	    
-		this.setSize(1660, 720);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.add(logo);
-		this.add(title);
-		this.add(ref);
-		this.add(Box.createVerticalGlue());
 
-		//this.setResizable(false);
-	
+        // Window icon
+        ImageIcon mainIcon = new ImageIcon(getClass().getResource("Kevin.png"));
+        Image img = mainIcon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        mainIcon = new ImageIcon(img);
+        this.setIconImage(mainIcon.getImage());
+
+        // Root panel with BoxLayout
+        JPanel rootPanel = new JPanel();
+        rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.PAGE_AXIS));
+        rootPanel.setBackground(new Color(29, 22, 26)); // Match background
+        rootPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Logo
+        ImageIcon iconCh3 = new ImageIcon(getClass().getResource("chapter3icon.png"));
+        JLabel logo = new JLabel(iconCh3);
+        logo.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Title label
+        JLabel title = new JLabel("Welcome to the world-renowned Celestial Resort Hotel!");
+        title.setForeground(new Color(100, 100, 100));
+        title.setFont(new Font("MV Boli", Font.PLAIN, 40));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Reference label
+        JLabel ref = new JLabel("yes, this is a Celeste reference");
+        ref.setForeground(new Color(95, 100, 100));
+        ref.setFont(new Font("MV Boli", Font.ITALIC, 20));
+        ref.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Buttons
+        JButton book = new JButton("Book Now!");
+        JButton loginStaff = new JButton("Staff Login");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+        buttonPanel.setOpaque(false);
+        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        buttonPanel.add(book);
+        buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        buttonPanel.add(loginStaff);
+
+        // Add components with spacing
+        rootPanel.add(Box.createVerticalStrut(30));
+        rootPanel.add(logo);
+        rootPanel.add(Box.createVerticalStrut(20));
+        rootPanel.add(title);
+        rootPanel.add(Box.createVerticalStrut(10));
+        rootPanel.add(ref);
+        rootPanel.add(Box.createVerticalGlue());
+        rootPanel.add(buttonPanel);
+        rootPanel.add(Box.createVerticalStrut(30));
+        rootPanel.add(Box.createVerticalGlue());
+
+        // Add rootPanel to frame
+        this.setContentPane(rootPanel);
+        this.setSize(1660, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null); // center on screen
 	}
 	
 	public static void main(String args[]) {
--- src/hotel/test.java
+++ src/hotel/test.java
@@ -2,49 +2,79 @@ package hotel;
 import java.awt.*;
 import javax.swing.*;
 
-/**
- * Main Window class 
- */
-
 public class test extends JFrame {
-	
-	
-	//Baseado no capítulo 3 de Celeste
-	public test () {
-		super("Celestial Resort Hotel");
-		
-		this.setLayout(null);
-		this.setSize(1080, 720);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//this.setResizable(false);
-		
-		ImageIcon  mainIcon = new ImageIcon("Kevin.jpeg");	//Main window icon
-		this.setIconImage(mainIcon.getImage());
-		this.getContentPane().setBackground(new Color(29, 22, 26));
-		this.setLayout(new BorderLayout());
-		
-		JLabel logo = new JLabel();
-		logo.setIcon(mainIcon);
-		logo.setSize(800, 800);
-	    logo.setHorizontalAlignment(SwingConstants.CENTER);
-	    this.add(logo);
-		
-		JLabel title = new JLabel("Welcome to the world-renowed Celestial Resort Hotel");
-		title.setSize(800, 800);
-	    title.setHorizontalAlignment(SwingConstants.CENTER);
-	    this.add(title);
-	    
-	    JLabel ref = new JLabel("yes, this is a Celeste reference");
-		ref.setBounds(300, 40, 200, 80);
-	    ref.setHorizontalAlignment(SwingConstants.CENTER);
-	    this.add(ref);
-	
-	}
-	
-	public static void main(String args[]) {
-		test window = new test();
-		window.setVisible(true);
-	}
+
+    public test() {
+        super("Celestial Resort Hotel");
+
+        // Window icon
+        ImageIcon mainIcon = new ImageIcon(getClass().getResource("Kevin.png"));
+        Image img = mainIcon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        mainIcon = new ImageIcon(img);
+        this.setIconImage(mainIcon.getImage());
+
+        // Root panel with BoxLayout
+        JPanel rootPanel = new JPanel();
+        rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.PAGE_AXIS));
+        rootPanel.setBackground(new Color(29, 22, 26)); // Match background
+        rootPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Logo
+        JLabel logo = new JLabel(mainIcon);
+        logo.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Title label
+        JLabel title = new JLabel("Welcome to the world-renowned Celestial Resort Hotel!");
+        title.setForeground(new Color(100, 100, 100));
+        title.setFont(new Font("MV Boli", Font.PLAIN, 40));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Reference label
+        JLabel ref = new JLabel("yes, this is a Celeste reference");
+        ref.setForeground(new Color(95, 100, 100));
+        ref.setFont(new Font("MV Boli", Font.ITALIC, 20));
+        ref.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Buttons
+        JButton book = new JButton("Book Now!");
+        JButton loginStaff = new JButton("Staff Login");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+        buttonPanel.setOpaque(false);
+        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        buttonPanel.add(book);
+        buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        buttonPanel.add(loginStaff);
+
+        // Add components with spacing
+        rootPanel.add(Box.createVerticalStrut(30));
+        rootPanel.add(logo);
+        rootPanel.add(Box.createVerticalStrut(20));
+        rootPanel.add(title);
+        rootPanel.add(Box.createVerticalStrut(10));
+        rootPanel.add(ref);
+        rootPanel.add(Box.createVerticalGlue());
+        rootPanel.add(buttonPanel);
+        rootPanel.add(Box.createVerticalStrut(30));
+        rootPanel.add(Box.createVerticalGlue());
+
+        // Add rootPanel to frame
+        this.setContentPane(rootPanel);
+        this.setSize(1000, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null); // center on screen
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            test window = new test();
+            window.setVisible(true);
+        });
+    }
 }
+
+
+
 
 
